4038e0fd4131ec88be6a9e22d1032de52ea08541
Diego P <diego_lpaez@hotmail.com>
2025-09-07 13:14:12 -0300

    tienda + integracion  mp

---
+++ JavaScript/E-Commerce2025/Client/js/products.js
@@ -0,0 +1,33 @@
+const productos = [
+  {
+    id: 1,
+    productName: "Nike",
+    price: 1000,
+    quanty: 1,
+    img: "../assets/img1.jpeg",
+  },
+
+  {
+    id: 2,
+    productName: "Adidas",
+    price: 800,
+    quanty: 1,
+    img: "../assets/img2.jpeg",
+  },
+
+  {
+    id: 3,
+    productName: "Puma",
+    price: 800,
+    quanty: 1,
+    img: "../assets/img3.jpeg",
+  },
+
+  {
+    id: 4,
+    productName: "New Balance",
+    price: 700,
+    quanty: 1,
+    img: "../assets/img4.jpeg",
+  }
+];
