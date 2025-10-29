235b8d5447dc54ae0eb573e1a097bbbd76d3daac
LucOrt91 <103439193+LucOrt91@users.noreply.github.com>
2025-10-29 00:42:26 -0300

    clases 5-11

---
+++ JavaScript/Stack PERN/frontend/src/components/NavBar/navigation.js
@@ -0,0 +1,17 @@
+export const PublicRoutes = [
+
+    { name: 'Home', path: '/' },
+    { name: 'About', path: '/about' },
+    { name: 'Login', path: '/login' },
+    { name: 'Register', path: '/register' },
+];
+
+export const PrivateRoutes = [
+
+    { name: 'Perfil', path: '/perfil' },
+    { name: 'Tareas', path: '/tareas' },
+    { name: 'Tareas/crear', path: '/tareas/crear' },
+    { name: 'Tareas/editar/:id', path: '/tareas/editar/:id' },
+    { name: 'Tareas/:id', path: '/tareas/:id' },
+
+];
